e7c903e4dce2906cf78778750c6e9ea0f7748204
AndresImery <andresimery@gmail.com>
2024-03-10 10:29:10 -0400

    feat(views): start on ClubView

--- src/views/ClubView.jsx
+++ src/views/ClubView.jsx
@@ -1,12 +1,20 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
 import Content from "../components/Content";
+import Card from '../components/Card';
+// import { useEffect, useState } from "react";
+import { useClubs } from '../hooks/useClubs'
 
 export default function ClubView() {
+
+  const {clubs, isLoading} = useClubs()
+
   return (
     <div>
       <Navbar />
       <Content>
         <h1>Mis clubs page</h1>
+        
+        {isLoading ? <div>Cargando...</div> : clubs.map((club) => <Card key={club.nombre} type="club" element={club} />)}
       </Content>
     </div>
   );
