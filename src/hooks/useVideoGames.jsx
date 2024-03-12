457dbdcff297c4330d5881c03ca533a2e52a6f29
AndresImery <andresimery@gmail.com>
2024-03-11 23:38:32 -0400

    Fix(clean): clean code

--- src/hooks/useVideoGames.jsx
+++ src/hooks/useVideoGames.jsx
@@ -5,16 +5,11 @@ export function useVideoGames(ids) {
     const [videogames, setVideoGames] = useState([])
 
     async function cargarData() {
-        // console.log(ids.length)
         var videojuegos = []
-        // console.log(ids.length)
         for (let index = 0; index < (await ids).length; index++) {
             const videogame = await getVideoGameWithId(ids[index])
-            // console.log(videogame)
             videojuegos.push(videogame)
         }
-        // console.log("here")
-        // console.log(videojuegos.map((videogame) => {videogame.id}))
         setVideoGames(await videojuegos)
     }
 
@@ -26,8 +21,6 @@ export function useVideoGames(ids) {
 
     if (!isLoading) {
         videogames.map((videogame) => console.log(videogame.data().titulo))
-
-        // console.log(hi)
     }
 
     return { videogames, setVideoGames, isLoading }
