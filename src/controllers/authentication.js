cdd558ef29be2a381a81de551d5c8b6720c63ab3
Danielito <daniel.deoliveira@correo.unimet.edu.ve>
2024-03-12 00:04:16 -0400

    MUERTE

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -4,6 +4,26 @@ import { addDoc, collection, deleteDoc, doc, getDocs, setDoc, query, where } fro
 import { auth, db } from "../firebase"
 import { useEffect, useState } from "react";
 
+function generateRandomUsername(firstName, lastName) {
+    const initials = (firstName.charAt(0) + lastName.charAt(0)).toLowerCase();
+    const randomString = Math.random().toString(36).substring(2, 20);
+    const username = initials + randomString;
+    return username;
+}
+
+export async function getUserByEmail(email) {
+    const userCollections = collection(db, 'users');
+    const userQuery = query(userCollections, where('email', '==', email))
+    const userSnapshot = await getDocs(userQuery)
+
+    if (userSnapshot.empty) {
+        return null;
+    }
+
+    const user = userSnapshot.docs[0].data(); 
+    return user;
+}
+
 export function emailLogin(email, password) {
     signInWithEmailAndPassword(auth, email, password)
 }
@@ -20,12 +40,12 @@ export async function createUser(firstname, lastname, email, uid) {
         "nombre": firstname,
         "apellido": lastname,
         "email": email,
+        "username": generateRandomUsername(firstname, lastname),
 
         "videojuego_preferido": "",
-        "username": "",
     };
     await addDoc(usersCollection, data)
-    return true
+    window.location.href = "/"
 }
 
 export function useAuth() {
--- src/hooks/useNewClub.jsx
+++ src/hooks/useNewClub.jsx
@@ -2,14 +2,13 @@ import { createClub } from "../controllers/club";
 
 export function useNewClub() {
 
-
     async function loadDatas(nombre, descripcion) {
         await createClub({ nombre: nombre, descripcion: descripcion, videojuegos: [] })
         window.location.href = "/clubs"
     }
 
     function addClub(nombre, descripcion) {
-        if (typeof descripcion!== "undefined") {
+        if (typeof descripcion !== "undefined") {
             loadDatas(nombre, descripcion)
         } else {
             console.log("No se pudo agregar el club")
